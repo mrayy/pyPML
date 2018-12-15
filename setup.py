22c943a40df50826cb352bd0c76b2f482fe58143
MHD Yamen Saraiji <mrayyamen@gmail.com>
2018-12-15 21:27:47 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+
+    test_extras = {
+    }
+except ImportError:
+    from distutils.core import setup
+
+    test_extras = {}
+
+setup(
+    name='py-PML',
+    version='1.0.0',
+    author='Yamen Saraiji',
+    author_email='yamen@kmd.keio.ac.jp',
+    description=(
+        'Practical Machine Learning Library for Python'),
+    #long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/mrayy/pyPML',
+    platforms='any',
+    packages=[
+        'PML',
+    ],
+    keywords='Practical Machine Learning',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: Freely Distributable',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Networking',
+    ],
+    install_requires=[
+          'pandas',
+          'matplotlib',
+          'numpy',
+          'ipywidgets'
+      ],
+    dependency_links=['https://github.com/attwad/python-osc'],
+    **test_extras
+)
